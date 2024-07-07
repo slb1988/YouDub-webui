bd118cb1b510ededa0b3e6dee7a5d4f3fb1574c5
laibing.sun <sunlaibing88@gmail.com>
2024-07-08 06:03:47 +0800

    Update pyannote_test.py

--- tests/pyannote_test.py
+++ tests/pyannote_test.py
@@ -1,6 +1,8 @@
 # instantiate the model
+import os
+
 from pyannote.audio import Model
 model = Model.from_pretrained(
   "pyannote/segmentation-3.0",
-  use_auth_token="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")
+  use_auth_token=os.getenv('HF_TOKEN'))
 print(model)
